b7131dcee89ec476496cfa578991c1c3da41b189
James Edward Gray II <james@graysoftinc.com>
2016-09-14 19:41:04 -0500

    9/14/16 stream.

--- ready_droplet.exs
+++ ready_droplet.exs
@@ -54,6 +54,41 @@ defmodule DigitalOcean do
   def smallest_size(region) do
     hd(region.sizes)
   end
+
+  def create_droplet!(props) do
+    DigOc.Droplet.new!(props)
+  end
+
+  def delete_droplet!(droplet) do
+    DigOc.Droplet.delete!(droplet.id)
+  end
+end
+
+defmodule Launcher do
+  def launch_unless_running(props) do
+    DigOc.droplets!.droplets
+    |> Enum.find(fn droplet -> droplet.name == props.name end)
+    |> do_launch_unless_running(props)
+  end
+
+  defp do_launch_unless_running(nil, props) do
+    IO.puts "Launching #{props.name}..."
+    DigitalOcean.create_droplet!(props)
+  end
+  defp do_launch_unless_running(
+    %{image: %{slug: r_image}, region: %{slug: r_region}, size: %{slug: r_size}},
+    props = %{image: p_image, region: p_region, size: p_size}
+  )
+  when r_image == p_image and r_region == p_region and r_size == p_size do
+    IO.puts "#{props.name} is already running."
+    :ok
+  end
+  defp do_launch_unless_running(running, props) do
+    IO.puts "Shutting down #{props.name}..."
+    DigitalOcean.delete_droplet!(running)
+    IO.puts "Relaunching #{props.name}..."
+    DigitalOcean.create_droplet!(props)
+  end
 end
 
 image = DigitalOcean.image_for_distribution_and_version!("Debian", "8.5 x64")
@@ -62,11 +97,11 @@ region =
   |> DigitalOcean.maximize_features
   |> DigitalOcean.favor_region(["New York", "San Fran"])
 size = DigitalOcean.smallest_size(region)
-
-DigOc.Droplet.new!( %{
+props = %{
   name: "elixir-launched",
   image: image.slug,
   region: region.slug,
   size: size
-} )
-|> IO.inspect
+}
+
+Launcher.launch_unless_running(props)
